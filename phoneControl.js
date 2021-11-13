c01fcbaaa2fa1b0db55385c3268fb84d30cd2f50
d-curley <curley.devyn@gmail.com>
2021-11-12 22:04:42 -0500

    Revert back to original tutorial code

--- phoneControl.js
+++ phoneControl.js
@@ -1,3 +1,4 @@
+
 var px = 50; // Position x and y
 var py = 50;
 var vx = 0.0; // Velocity x and y
@@ -5,61 +6,39 @@ var vy = 0.0;
 var updateRate = 1/60; // Sensor refresh rate
 
 function getAccel(){
-  document.getElementById("f2b").innerHTML = "checkcheck2";//(Math.round( frontToBack_degrees* 100) / 100).toString();
-DeviceMotionEvent.requestPermission().then(response => {
-  if (response == 'granted') {
-      console.log("accelerometer permission granted");
+    DeviceMotionEvent.requestPermission().then(response => {
+        if (response == 'granted') {
+       // Add a listener to get smartphone orientation 
+           // in the alpha-beta-gamma axes (units in degrees)
+            window.addEventListener('deviceorientation',(event) => {
+                // Expose each orientation angle in a more readable way
+                rotation_degrees = event.alpha;
+                frontToBack_degrees = event.beta;
+                leftToRight_degrees = event.gamma;
+                
+                // Update velocity according to how tilted the phone is
+                // Since phones are narrower than they are long, double the increase to the x velocity
+                vx = vx + leftToRight_degrees * updateRate*2; 
+                vy = vy + frontToBack_degrees * updateRate;
+                
+                // Update position and clip it to bounds
+                px = px + vx*.5;
+                if (px > 98 || px < 0){ 
+                    px = Math.max(0, Math.min(98, px)) // Clip px between 0-98
+                    vx = 0;
+                }
 
-     // Add a listener to get smartphone acceleration 
-    // in the XYZ axes (units in m/s^2)
-    //how else can we access this "event"?
-    //window.addEventListener('devicemotion', (event) => {
-        
-    //});
-
-    // Add a listener to get smartphone orientation 
-   // in the alpha-beta-gamma axes (units in degrees)
-     window.addEventListener('deviceorientation',(event) => {
-         rotation_degrees = event.alpha;
-        frontToBack_degrees = event.beta;
-        leftToRight_degrees = event.gamma;
-
-        // Since phones are narrower than they are long, 
-        // double the increase to the x velocity
-        vx = vx + leftToRight_degrees * updateRate; 
-        vy = vy + frontToBack_degrees * updateRate*.5;
-        
-        // velocity affect is halved for smoothing
-        px = px + vx*.25;
-        if (px > 98 || px < 0){ 
-            px = Math.max(0, Math.min(98, px)) // Clip px between 0-98
-            vx = 0;
+                py = py + vy*.5;
+                if (py > 98 || py < 0){
+                    py = Math.max(0, Math.min(98, py)) // Clip py between 0-98
+                    vy = 0;
+                }
+                
+                dot = document.getElementsByClassName("indicatorDot")[0]
+                dot.setAttribute('style', "left:" + (px) + "%;" +
+                                              "top:" + (py) + "%;");
+                
+            });
         }
-        py = py + vy*.25;
-        if (py > 98 || py < 0){ 
-            py = Math.max(0, Math.min(98, py)) // Clip py between 0-98
-            vy = 0;
-        }
-
-        // dot = document.getElementsByClassName("indicatorDot")[0]
-        // dot.setAttribute('style', "left:" + (px) + "%;" +
-        //               "top:" + (py) + "%;");
-
-        
-
-        pixelVal= (Math.round(leftToRight_degree) -50)/10;    
-        document.getElementById("l2r").innerHTML = pixelVal.toString();
-      
     });
-  }
-});
 }
-
-// function pixelSend(val){
-//   var xhr = new XMLHttpRequest();
-//         xhr.open("POST", "/data", true);
-//         xhr.setRequestHeader('Content-Type', 'application/json');
-//         xhr.send(JSON.stringify({
-//             value: val
-//         }));
-// }
